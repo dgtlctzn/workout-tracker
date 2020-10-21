1a76abe54ab1a4bdf3f53ba6f9c10f5c89dad451
Joseph Perry <josephperry720@gmail.com>
2020-10-21 16:27:15 -0400

    created post route for workouts

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -9,7 +9,21 @@ router.get("/api/workouts", (req, res) => [
         console.log(err);
         res.json({
             error: true,
+            data: null,
             message: "failed to get workout list"
+        })
+    })
+])
+
+router.post("/api/workouts", (req, res) => [
+    db.Workout.create(req.body).then(postedWorkout => {
+        res.json(postedWorkout);
+    }).catch(err => {
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "failed to post workout"
         })
     })
 ])
--- models/Workout.js
+++ models/Workout.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        required: true,
+  day: {
+    type: Date,
+    required: true,
+  },
+  exercises: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Exercise",
     },
-    exercise: {
-        type: Schema.Types.ObjectId,
-        ref: "Exercise"
-    }
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
