95e5cb37ececfa4406dd72fb946e8b9f9831bfd9
Joseph Perry <josephperry720@gmail.com>
2020-10-21 14:32:49 -0400

    added connection to mongoose

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/api/config", (req, res) => {
-    res.json({
-        success: true
-    })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose connection success!");
 })
 
-app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`)
+connection.on("error", () => {
+    console.log("mongoose error!")
 })
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
