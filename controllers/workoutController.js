7bb24dc29151b513b3c6f1ea7e491db7d854de21
Joseph Perry <josephperry720@gmail.com>
2020-10-22 17:13:58 -0400

    updated populated method

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -2,43 +2,71 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req, res) => [
-    db.Workout.find().populate("Exercise").then(foundWorkouts => {
-        res.json(foundWorkouts);
-    }).catch(err => {
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "failed to get workout list"
-        })
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .populate("exercises")
+    .then((foundWorkouts) => {
+      res.json(foundWorkouts);
     })
-])
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "failed to get workout list",
+      });
+    });
+});
 
-router.post("/api/workouts", (req, res) => [
-    db.Workout.create(req.body).then(postedWorkout => {
-        res.json(postedWorkout);
-    }).catch(err => {
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "failed to post workout"
-        })
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((postedWorkout) => {
+      res.json(postedWorkout);
     })
-])
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "failed to post workout",
+      });
+    });
+});
 
-router.get("/api/workouts/range", (req, res) => [
-    db.Workout.find().populate("Exercise").then(foundWorkouts => {
-        res.json(foundWorkouts);
-    }).catch(err => {
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "failed to get workout list"
-        })
+router.put("/api/workouts/:id", (req, res) => {
+  db.Exercise.create(req.body).then(exercise => {
+//     console.log(exercise);
+    console.log(req.body);
+    db.Workout.updateOne({_id: req.params.id}, { $push: {exercises: exercise}})
+    .then((updatedWorkout) => {
+      res.json(updatedWorkout);
+      console.log(updatedWorkout)
     })
-])
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "failed to update workout",
+      });
+    });
+  })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find()
+    .populate("exercises")
+    .then((foundWorkouts) => {
+      res.json(foundWorkouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "failed to get workout list",
+      });
+    });
+});
 
 module.exports = router;
